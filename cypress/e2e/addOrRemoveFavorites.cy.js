5405468f28dc4200ec9f17f2985888550c33ff59
jBoeBitus <jonathan.boe@bituslabs.com>
2023-08-01 17:08:58 -0700

    defined tests

--- cypress/e2e/addOrRemoveFavorites.cy.js
+++ cypress/e2e/addOrRemoveFavorites.cy.js
@@ -1,15 +1,17 @@
 /// <reference types="cypress" />
 
-describe('Registers account inputs', () => {
+describe('add and remove favorite spots', () => {
 	beforeEach(() => {});
 
-	it('', () => {});
-	it('', () => {});
-	it('', () => {});
-	it('', () => {});
-	it('', () => {});
-	it('', () => {});
-	it('', () => {});
+	it('open modal on click', () => {});
+	it('close modal on click off', () => {});
+	it('close modal on X click', () => {});
+
+	it('check we are rendering the correct favorite spots', () => {});
+	it('check that our starting favorites are checked', () => {});
+	it('make sure our checkboxes get checked and unchecked on click', () => {});
+	it('make sure when we cancel it does not save those spots', () => {});
+	it('make sure our spots change when we want to save new spots', () => {});
 
 	// We should test our validation
 });
